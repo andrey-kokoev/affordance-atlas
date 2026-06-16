f804eaf9567060a28d4ab56cc2675c00541d3aa7
Andrey Kokoev <andrei.kokoev@gmail.com>
2026-06-16 15:51:59 -0500

    Switch Vite plugin from React to Vue

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,6 @@
-import react from "@vitejs/plugin-react";
+import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [vue()],
 });
